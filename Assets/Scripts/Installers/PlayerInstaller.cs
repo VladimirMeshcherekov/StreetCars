0590c71075e75d43d80b8d0d33ab8e530a44a27f
snapp <snapp@test.com>
2023-08-28 20:54:53 +0300

    Added mobile input

--- Assets/Scripts/Installers/PlayerInstaller.cs
+++ Assets/Scripts/Installers/PlayerInstaller.cs
@@ -1,5 +1,6 @@
 using Generate_Environment;
 using Player.Input;
+using SimpleInputNamespace;
 using UnityEngine;
 using Zenject;
 
@@ -8,12 +9,42 @@ namespace Installers
     public class PlayerInstaller : MonoInstaller
     {
         [SerializeField] private EnvironmentSpawner environmentSpawner;
+        [SerializeField] private Joystick playerJoystick;
+        private EventBus _eventBus;
+
         public override void InstallBindings()
         {
-            var EventBus = new EventBus();
-            var _input =  new StandalonePlayerInput(EventBus);
+            InstallArchitectureComponents();
+            InstallInput();
+            InstallLevelGeneration();
+        }
+
+        private void InstallArchitectureComponents()
+        {
+            _eventBus = new EventBus();
+            Container.BindInstance(_eventBus).AsSingle();
+        }
+
+        private void InstallInput()
+        {
+            InstallMobileInput();
+            InstallStandaloneInput();
+        }
+
+        private void InstallLevelGeneration()
+        {
             Container.BindInstance(environmentSpawner).AsSingle();
-            Container.BindInstance(EventBus).AsSingle();;
+        }
+
+        private void InstallStandaloneInput()
+        {
+            var standalonePlayerInput = new StandalonePlayerInput(_eventBus);
+        }
+
+        private void InstallMobileInput()
+        {
+            var joystickPlayerInput = new JoystickPlayerInput(_eventBus, playerJoystick);
+            Container.Bind<ITickable>().FromInstance(joystickPlayerInput).AsSingle().NonLazy();
         }
     }
 }
---
+++ Assets/Scripts/Player/Input/JoystickPlayerInput.cs
@@ -0,0 +1,23 @@
+﻿using SimpleInputNamespace;
+using UnityEngine;
+using Zenject;
+
+namespace Player.Input
+{
+    public class JoystickPlayerInput : ITickable
+    {
+        private EventBus _eventBus;
+        private Joystick _joystick;
+        
+        public JoystickPlayerInput(EventBus eventBus, Joystick joystick)
+        {
+            _eventBus = eventBus;
+            _joystick = joystick;
+        }
+
+        public void Tick()
+        {
+             _eventBus.Invoke(new PlayerInputChangedSignal(_joystick.Value));
+        }
+    }
+}
