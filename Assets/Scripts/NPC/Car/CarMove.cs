ee9f922b313a9e96edccfde1d917ca63e1408b4f
snapp <snapp@test.com>
2023-08-29 20:17:18 +0300

    added npc car move and npc cars prefabs

---
+++ Assets/Scripts/NPC/Car/CarMove.cs
@@ -0,0 +1,45 @@
+using DG.Tweening;
+using UnityEngine;
+
+namespace NPC.Car
+{
+    public class CarMove : MonoBehaviour
+    {
+        [SerializeField] private float viewDistance;
+        [SerializeField] private float speed;
+        [SerializeField] private bool showViewRay;
+        private bool _isAbleToMove = true;
+
+        private void FixedUpdate()
+        {
+            _isAbleToMove = true;
+
+            if (Physics.Raycast(transform.position + Vector3.up, transform.TransformDirection(Vector3.right), out var hit, viewDistance))
+            {
+                if (hit.collider.gameObject.TryGetComponent(out CarMove carMove))
+                {
+                    _isAbleToMove = false;
+                }
+            }
+
+            Move();
+            DebugShowViewRay();
+        }
+
+        private void Move()
+        {
+            if (_isAbleToMove)
+            {
+                Tween tween = transform.DOMove(transform.position + transform.right, 1 / speed);
+            }
+        }
+
+        private void DebugShowViewRay()
+        {
+            if (showViewRay)
+            {
+                Debug.DrawRay(transform.position, new Vector3(viewDistance, 0, 0), Color.magenta, Time.deltaTime);
+            }
+        }
+    }
+}
